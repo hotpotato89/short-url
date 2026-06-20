1769884641070bbdad1bf7d09b9c8cb1a7345f02
hotpotato <ramilreading@gmail.com>
2026-06-20 19:22:39 +0300

    feat: connected me function

--- src/app/api/deps.py
+++ src/app/api/deps.py
@@ -1,16 +1,23 @@
 from fastapi import Depends
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from typing import Annotated, AsyncGenerator
+from typing import Annotated, Any, AsyncGenerator
 
 from src.app.core.database import SessionLocal
+from src.app.core.exceptions import InvalidTokenError
+from src.app.models.user import User
 from src.app.repositories.user_repository import UserRepository
 from src.app.services.user_service import UserService
+from src.app.utils.jwt import decode_jwt
 
 
 async def get_session() -> AsyncGenerator[AsyncSession, None]:
     async with SessionLocal() as session:
         yield session
+
+
+# Repository dependencies
 
 
 async def get_user_repo(
@@ -19,8 +26,32 @@ async def get_user_repo(
     return UserRepository(session)
 
 
+# Service dependencies
+
+
 async def get_user_service(
     user_repo: Annotated[UserRepository, Depends(get_user_repo)],
     session: Annotated[AsyncSession, Depends(get_session)],
 ) -> UserService:
     return UserService(user_repo, session)
+
+
+# Jwt dependencies
+
+security = HTTPBearer(auto_error=True)
+
+
+async def get_token_payload(
+    token: Annotated[HTTPAuthorizationCredentials, Depends(security)],
+) -> dict[str, Any]:
+    payload = decode_jwt(token.credentials)
+    if payload["type"] != "access":
+        raise InvalidTokenError("Invalid token type")
+    return payload
+
+
+async def get_current_user(
+    user_data: Annotated[dict[str, Any], Depends(get_token_payload)],
+    repo: Annotated[UserRepository, Depends(get_user_repo)],
+) -> User:
+    return await repo.get_user(user_data["email"])
--- src/app/api/routers/auth.py
+++ src/app/api/routers/auth.py
@@ -2,7 +2,8 @@ from typing import Annotated
 
 from fastapi import APIRouter, Depends
 
-from src.app.api.deps import get_user_service
+from src.app.api.deps import get_current_user, get_user_service
+from src.app.models.user import User
 from src.app.schemas.token import TokenInfo
 from src.app.schemas.user import UserLogin, UserRegister, UserResponse
 from src.app.services.user_service import UserService
@@ -24,3 +25,8 @@ async def login(
     service: Annotated[UserService, Depends(get_user_service)], login_data: UserLogin
 ) -> TokenInfo:
     return await service.login(login_data)
+
+
+@router.get("/me")
+async def me(current_user: Annotated[User, Depends(get_current_user)]) -> UserResponse:
+    return UserResponse.model_validate(current_user)
