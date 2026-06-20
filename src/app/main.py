01ff7f48832d3ac2e167fc3c71bee1bc2acac4f0
hotpotato <ramilreading@gmail.com>
2026-06-20 15:40:44 +0300

    feat: used colorful formatter

--- src/app/main.py
+++ src/app/main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 from src.app.core.lifespan import lifespan
 from src.app.core.logging import setup_logging
 
-setup_logging('INFO')
+setup_logging("INFO")
 
 
 app = FastAPI(title="Short Url", lifespan=lifespan)
