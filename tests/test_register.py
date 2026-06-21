2cd5073fe59084b0d4930f4747881a0918ba0797
hotpotato <ramilreading@gmail.com>
2026-06-21 19:46:29 +0300

    feat: added other register tests

--- tests/test_register.py
+++ tests/test_register.py
@@ -1,5 +1,6 @@
 import pytest
 import httpx
+from fastapi import status
 
 from src.app.schemas.user import UserResponse
 
@@ -15,7 +16,24 @@ async def test_register(client, fake_user) -> None:
 
     data = UserResponse(**resp.json())
 
-    assert resp.status_code == 200
+    assert resp.status_code == status.HTTP_200_OK
     assert data.email == fake_user.email
     assert hasattr(data, "created_at")
     assert hasattr(data, "id")
+
+
+async def test_register_invalid(client) -> None:
+    resp = await client.post("/auth/register", json={})
+
+    assert resp.status_code == status.HTTP_422_UNPROCESSABLE_CONTENT
+
+
+async def test_register_conflict(client, fake_user) -> None:
+    user = {
+        "email": fake_user.email,
+        "password": fake_user.password.get_secret_value(),
+    }
+    await client.post("/auth/register", json=user)
+    resp = await client.post("/auth/register", json=user)
+
+    assert resp.status_code == status.HTTP_409_CONFLICT
