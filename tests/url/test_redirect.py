140307892f984aa4e3f6b692b1a64b218d6ae318
hotpotato <ramilreading@gmail.com>
2026-06-22 13:48:31 +0300

    feat: added other redirect tests

--- tests/url/test_redirect.py
+++ tests/url/test_redirect.py
@@ -19,4 +19,17 @@ async def test_redirect(client: AsyncClient, auth_token: str) -> None:
     redirect_resp = await client.get(f"/url/{url_data.slug}", follow_redirects=False)
 
     assert redirect_resp.status_code == status.HTTP_307_TEMPORARY_REDIRECT
-    assert redirect_resp.headers['Location'] == 'https://google.com/'
+    assert redirect_resp.headers["Location"] == "https://google.com/"
+
+
+async def test_redirect_not_found(client: AsyncClient) -> None:
+    redirect_resp = await client.get("/url/notexist", follow_redirects=False)
+
+    assert redirect_resp.status_code == status.HTTP_404_NOT_FOUND
+
+
+async def test_redirect_invalid_slug(client: AsyncClient) -> None:
+    invalid_slug = "verylong" * 21  # max slug size = 20
+    redirect_resp = await client.get(f"/url/{invalid_slug}", follow_redirects=False)
+
+    assert redirect_resp.status_code == status.HTTP_422_UNPROCESSABLE_CONTENT
