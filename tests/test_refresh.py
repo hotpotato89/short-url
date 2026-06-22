bcb419f803d2baeacb3a109cadd5fabe08580fcc
hotpotato <ramilreading@gmail.com>
2026-06-22 19:34:27 +0300

    feat: added refresh token tests

---
+++ tests/test_refresh.py
@@ -0,0 +1,47 @@
+from httpx import AsyncClient
+import pytest
+from fastapi import status
+
+from src.app.schemas.token import TokenInfo
+
+
+async def test_refresh(client: AsyncClient, auth_tokens: TokenInfo) -> None:
+    refresh_resp = await client.post(
+        "/auth/refresh", json={"refresh_token": auth_tokens.refresh_token}
+    )
+
+    refresh_data = TokenInfo(**refresh_resp.json())
+
+    assert hasattr(refresh_data, "access_token")
+    assert hasattr(refresh_data, "refresh_token")
+    assert hasattr(refresh_data, "type")
+
+
+async def test_refresh_invalid(client: AsyncClient) -> None:
+    refresh_resp = await client.post(
+        "/auth/refresh", json={"refresh_token": "not-a-token"}
+    )
+
+    assert refresh_resp.status_code == status.HTTP_401_UNAUTHORIZED
+
+
+async def test_refresh_invalid_type(
+    client: AsyncClient, auth_tokens: TokenInfo
+) -> None:
+    refresh_resp = await client.post(
+        "/auth/refresh", json={"refresh_token": auth_tokens.access_token}
+    )
+
+    assert refresh_resp.status_code == status.HTTP_401_UNAUTHORIZED
+
+
+async def test_refresh_reuse(client: AsyncClient, auth_tokens: TokenInfo) -> None:
+    refresh_resp1 = await client.post(
+        "/auth/refresh", json={"refresh_token": auth_tokens.refresh_token}
+    )
+    assert refresh_resp1.status_code == status.HTTP_200_OK
+
+    refresh_resp2 = await client.post(
+        "/auth/refresh", json={"refresh_token": auth_tokens.refresh_token}
+    )
+    assert refresh_resp2.status_code == status.HTTP_401_UNAUTHORIZED
