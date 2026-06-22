0dc6d1f1ff7ea0ceb3327a45c231d13d7da357f3
hotpotato <ramilreading@gmail.com>
2026-06-22 13:09:57 +0300

    feat: added login tests

---
+++ tests/test_login.py
@@ -0,0 +1,29 @@
+import pytest
+from fastapi import status
+
+from src.app.schemas.token import TokenInfo
+
+
+async def test_login(client, fake_user) -> None:
+    fake = {"email": fake_user.email, "password": fake_user.password.get_secret_value()}
+    await client.post("/auth/register", json=fake)
+    resp = await client.post("/auth/login", json=fake)
+
+    data = TokenInfo(**resp.json())
+
+    assert resp.status_code == status.HTTP_200_OK
+    assert hasattr(data, "access_token")
+    assert hasattr(data, "refresh_token")
+
+
+async def test_login_invalid(client) -> None:
+    resp = await client.post("/auth/login", json={})
+
+    assert resp.status_code == status.HTTP_422_UNPROCESSABLE_CONTENT
+
+
+async def test_login_invalid_credentials(client, fake_user) -> None:
+    fake = {"email": fake_user.email, "password": fake_user.password.get_secret_value()}
+    resp = await client.post("/auth/login", json=fake)
+
+    assert resp.status_code == status.HTTP_401_UNAUTHORIZED
