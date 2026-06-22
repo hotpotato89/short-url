79aae4352e33d77a0608cb3ada9b4122ed29f066
hotpotato <ramilreading@gmail.com>
2026-06-22 16:16:35 +0300

    feat: added refresh token repository

---
+++ src/app/repositories/refresh_token_reposiotry.py
@@ -0,0 +1,30 @@
+from sqlalchemy import select, delete
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.app.models.refresh_token import RefreshToken
+
+
+class RefreshTokenRepository:
+    def __init__(self, session: AsyncSession) -> None:
+        self.session = session
+
+    async def create(self, user_id: int, refresh_token: str) -> RefreshToken:
+        new_token = RefreshToken(user_id=user_id, token=refresh_token)
+        self.session.add(new_token)
+        await self.session.flush()
+        return new_token
+
+    async def get_by_token(self, refresh_token: str) -> RefreshToken | None:
+        result = await self.session.execute(
+            select(RefreshToken).where(RefreshToken.token == refresh_token)
+        )
+        token = result.scalar_one_or_none()
+        if not token:
+            return
+        return token
+
+    async def delete_by_token(self, refresh_token: str) -> None:
+        await self.session.execute(
+            delete(RefreshToken).where(RefreshToken.token == refresh_token)
+        )
+        await self.session.flush()
