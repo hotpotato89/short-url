c5f3edc5fbbdd9be1e2041195e01827eda13589a
hotpotato <ramilreading@gmail.com>
2026-06-22 13:43:20 +0300

    chore: added auth_token

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,7 +14,9 @@ from httpx import AsyncClient, ASGITransport
 from src.app.api.deps import get_session
 from src.app.main import app
 from src.app.models.base import Base
+from src.app.schemas.token import TokenInfo
 from src.app.schemas.user import UserRegister
+from src.app.utils.jwt import create_access_token
 
 
 faker = Faker()
@@ -61,3 +63,24 @@ async def client(db_session) -> AsyncGenerator[AsyncClient, None]:
 @pytest.fixture()
 async def fake_user() -> UserRegister:
     return UserRegister(email=faker.email(), password=SecretStr(faker.password()))
+
+
+@pytest.fixture()
+async def auth_token(client: AsyncClient, fake_user: UserRegister) -> str:
+    await client.post(
+        "/auth/register",
+        json={
+            "email": fake_user.email,
+            "password": fake_user.password.get_secret_value(),
+        },
+    )
+    login_resp = await client.post(
+        "/auth/login",
+        json={
+            "email": fake_user.email,
+            "password": fake_user.password.get_secret_value(),
+        },
+    )
+    login_data = TokenInfo(**login_resp.json())
+
+    return login_data.access_token
