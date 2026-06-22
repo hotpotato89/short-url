a11a8eabfb4cdfb71aae6d165d12f0685f66e289
hotpotato <ramilreading@gmail.com>
2026-06-22 16:16:50 +0300

    style: used ruff

--- src/app/models/refresh_token.py
+++ src/app/models/refresh_token.py
@@ -4,12 +4,11 @@ from sqlalchemy import ForeignKey, String
 from src.app.models.base import Base
 
 
-
 class RefreshToken(Base):
     __tablename__ = "refresh_tokens"
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    user_id: Mapped[int] = mapped_column(ForeignKey('users.id', ondelete='CASCADE'), nullable=False, index=True)
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey("users.id", ondelete="CASCADE"), nullable=False, index=True
+    )
     token: Mapped[str] = mapped_column(String(500), nullable=False, index=True)
-
-
