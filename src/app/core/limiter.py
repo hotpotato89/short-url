01f54c8e11b9dd1ae62f8bdd7a46527d8e401634
hotpotato <ramilreading@gmail.com>
2026-06-23 16:19:04 +0300

    chore: setted 10/minute

--- src/app/core/limiter.py
+++ src/app/core/limiter.py
@@ -7,5 +7,5 @@ from src.app.core.settings import settings
 limiter = Limiter(
     key_func=get_remote_address,
     storage_uri=settings.redis.rate_limiter_url,
-    default_limits=["100/hour"],
+    default_limits=['10/minute'],
 )
