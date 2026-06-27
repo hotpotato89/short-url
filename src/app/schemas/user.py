ba30def4fd5eb89d4119f721877bc778aa659e7f
hotpotato <ramilreading@gmail.com>
2026-06-27 14:23:08 +0300

    feat(user response schema): is_superadmin column

--- src/app/schemas/user.py
+++ src/app/schemas/user.py
@@ -31,5 +31,6 @@ class UserResponse(BaseModel):
     email: str
     created_at: datetime
     role: Literal["user", "admin"]
+    is_supeadmin: bool = False
 
     model_config = {"from_attributes": True}
