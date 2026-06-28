203b36e77aa8a607e1d7e9edc36fcb79b682edd2
hotpotato <ramilreading@gmail.com>
2026-06-28 22:02:46 +0300

    fix(crypt util): used sha256 instead of Fernet

--- src/app/utils/crypt.py
+++ src/app/utils/crypt.py
@@ -1,22 +1,8 @@
-from logging import getLogger
-
-from cryptography.fernet import Fernet
-
-from src.app.core.settings import settings
-
-
-logger = getLogger(__name__)
-
+from hashlib import sha256
 
 class CryptUtil:
-    def __init__(self) -> None:
-        self._fernet = Fernet(settings.fernet.secret_key.get_secret_value().encode())
-
-    def encrypt(self, token: str) -> str:
-        return self._fernet.encrypt(token.encode()).decode()
-
-    def decrypt(self, encrypted_token: str) -> str:
-        return self._fernet.decrypt(encrypted_token.encode()).decode()
+    def hash(self, token: str) -> str:
+        return sha256(token.encode()).hexdigest()
 
 
 crypt_util = CryptUtil()
