b6754408c2c2c88f85d0477a323287ce2ab582af
hotpotato <ramilreading@gmail.com>
2026-06-30 11:47:25 +0300

    chore(database): celery session factory (sync)

--- src/app/core/database.py
+++ src/app/core/database.py
@@ -1,3 +1,5 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, Session
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from src.app.core.settings import settings
 
@@ -12,6 +14,22 @@ engine = create_async_engine(
 SessionLocal = async_sessionmaker(
     engine,
     class_=AsyncSession,
+    expire_on_commit=False,
+    autoflush=False,
+    autocommit=False,
+)
+
+celery_engine = create_engine(
+    settings.db.url.replace("+asyncpg", ""),
+    pool_size=10,
+    max_overflow=15,
+    pool_pre_ping=True,
+    pool_recycle=60 * 60,
+)
+
+CelerySessionLocal = sessionmaker(
+    celery_engine,
+    class_=Session,
     expire_on_commit=False,
     autoflush=False,
     autocommit=False,
