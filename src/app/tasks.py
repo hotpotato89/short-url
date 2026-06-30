339136924cebe10b5981cb32d99e0f18e5ec4ad8
hotpotato <ramilreading@gmail.com>
2026-06-30 12:00:30 +0300

    feat: Celery tasks

---
+++ src/app/tasks.py
@@ -0,0 +1,11 @@
+from celery import shared_task
+
+from src.app.core.database import CelerySessionLocal
+from src.app.repositories.celery import CeleryRepository
+
+
+@shared_task
+def increment_clicks_task(slug: str) -> None:
+    with CelerySessionLocal() as session:
+        repo = CeleryRepository(session)
+        repo.increment_clicks(slug)
