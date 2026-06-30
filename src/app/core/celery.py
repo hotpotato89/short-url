24c5ee4def63b599656c24a073796dec7f64ffef
hotpotato <ramilreading@gmail.com>
2026-06-30 15:01:27 +0300

    fix(core/celery): imported ORM models

--- src/app/core/celery.py
+++ src/app/core/celery.py
@@ -2,6 +2,9 @@ from celery import Celery
 
 from src.app.core.settings import settings
 
+from src.app.models.user import User  # noqa
+import src.app.models  # noqa
+
 
 celery_app = Celery(
     "short_url", broker=settings.redis.celery_url, backend=settings.redis.celery_url
