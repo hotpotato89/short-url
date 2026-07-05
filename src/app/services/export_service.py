b7e724373123fc92d46cee8cada5c00ee825b4cf
hotpotato <ramilreading@gmail.com>
2026-07-05 12:02:22 +0300

    feat(export service): xlsx export

--- src/app/services/export_service.py
+++ src/app/services/export_service.py
@@ -1,7 +1,9 @@
 import csv
-from io import StringIO
 import json
+from io import BytesIO, StringIO
 from typing import Literal, Sequence
+
+import pandas as pd
 
 from src.app.models.short_url import ShortUrl
 from src.app.repositories.short_url_repository import ShortUrlRepository
@@ -11,27 +13,31 @@ class ExportService:
     def __init__(self, repo: ShortUrlRepository) -> None:
         self.repo = repo
 
-    async def export(self, format: Literal["csv", "json"] = "json") -> str:
+    async def export(
+        self, format: Literal["csv", "json", "xlsx"] = "json"
+    ) -> str | bytes:
         urls = await self.repo.get_all()
 
         if format == "csv":
             return self._csv_format(urls)
         elif format == "json":
             return self._json_format(urls)
+        elif format == "xlsx":
+            return self._xlsx_format(urls)
 
-    def _json_format(self, urls: Sequence) -> str:
-        data = []
-        for url in urls:
-            data.append(
-                {
-                    "id": url.id,
-                    "original_url": url.original_url,
-                    "slug": url.slug,
-                    "clicks": url.clicks,
-                    "owner_id": url.owner_id,
-                    "created_at": url.created_at.isoformat(),
-                }
-            )
+    def _json_format(self, urls: Sequence[ShortUrl]) -> str:
+        data = [
+            {
+                "id": url.id,
+                "original_url": url.original_url,
+                "slug": url.slug,
+                "clicks": url.clicks,
+                "owner_id": url.owner_id,
+                "created_at": url.created_at.isoformat(),
+                "expires_at": url.expires_at.isoformat() if url.expires_at else None,
+            }
+            for url in urls
+        ]
         return json.dumps(data, indent=2, ensure_ascii=False)
 
     def _csv_format(self, urls: Sequence[ShortUrl]) -> str:
@@ -39,7 +45,15 @@ class ExportService:
         writer = csv.writer(output)
 
         writer.writerow(
-            ["ID", "Original url", "Slug", "Clicks", "Owner ID", "Created at"]
+            [
+                "ID",
+                "Original URL",
+                "Slug",
+                "Clicks",
+                "Owner ID",
+                "Created At",
+                "Expires At",
+            ]
         )
 
         for url in urls:
@@ -51,7 +65,30 @@ class ExportService:
                     url.clicks,
                     url.owner_id,
                     url.created_at.isoformat(),
+                    url.expires_at.isoformat() if url.expires_at else None,
                 ]
             )
+
+        return output.getvalue()
+
+    def _xlsx_format(self, urls: Sequence[ShortUrl]) -> bytes:
+        data = [
+            {
+                "ID": url.id,
+                "Original URL": url.original_url,
+                "Slug": url.slug,
+                "Clicks": url.clicks,
+                "Owner ID": url.owner_id,
+                "Created At": url.created_at.isoformat(),
+                "Expires At": url.expires_at.isoformat() if url.expires_at else None,
+            }
+            for url in urls
+        ]
+
+        df = pd.DataFrame(data)
+        output = BytesIO()
+
+        with pd.ExcelWriter(output, engine="xlsxwriter") as writer:
+            df.to_excel(writer, sheet_name="URLs", index=False)
 
         return output.getvalue()
