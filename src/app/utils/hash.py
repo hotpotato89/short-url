dce59237dfde10c982a654681be65450b54123fc
Hotpotato89 <ramilreading@gmail.com>
2026-07-09 20:18:52 +0300

    Feature/argon2 tune (#42)
    * chore(settings): Argon2 settings
    
    * docs: arong2 settings to example env
    
    * feat(hash util): used argon2 settings
    
    * chore(ci): env values

--- src/app/utils/hash.py
+++ src/app/utils/hash.py
@@ -1,6 +1,16 @@
 from passlib.context import CryptContext
 
-pwd_context = CryptContext(["argon2"], deprecated="auto")
+from src.app.core.settings import settings
+
+pwd_context = CryptContext(
+    ["argon2"],
+    deprecated="auto",
+    argon2__time_cost=settings.argon2.time_cost,
+    argon2__memory_cost=settings.argon2.memory_cost,
+    argon2__parallelism=settings.argon2.parallelism,
+    argon2__hash_len=settings.argon2.hash_len,
+    argon2__salt_len=settings.argon2.salt_len,
+)
 
 
 def hash_password(password: str) -> str:
