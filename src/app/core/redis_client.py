bf7bcfe4032ada998d9b1af7d3b46b008acbdc78
Hotpotato89 <ramilreading@gmail.com>
2026-07-12 10:54:30 +0300

    fix(core/redis client): use standart logging (#44)
    * fix(core/redis client): use standart logging
    
    * docs(README): tests count

--- src/app/core/redis_client.py
+++ src/app/core/redis_client.py
@@ -1,10 +1,7 @@
 from redis.asyncio import Redis
 from simple_redis_cache.asyncio import Cache
 
-from src.app.core.logging import get_logger
 from src.app.core.settings import settings
 
-
-logger = get_logger(__name__)
 redis_client = Redis.from_url(settings.redis.cache_url, decode_responses=True)
-cache_manager = Cache(redis_client, logger)  # type: ignore
+cache_manager = Cache(redis_client)  # type: ignore
