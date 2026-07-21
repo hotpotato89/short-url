d2b1f2bdf5119ca9af72219d65224b457f2ff497
hotpotato <ramilreading@gmail.com>
2026-07-21 15:11:33 +0300

    fix(redis client): decode responses = False

--- src/app/core/redis_client.py
+++ src/app/core/redis_client.py
@@ -3,5 +3,5 @@ from simple_redis_cache.asyncio import Cache
 
 from src.app.core.settings import settings
 
-redis_client = Redis.from_url(settings.redis.cache_url, decode_responses=True)
+redis_client = Redis.from_url(settings.redis.cache_url, decode_responses=False)
 cache_manager = Cache(redis_client)  # type: ignore
