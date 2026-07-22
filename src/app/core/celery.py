29f7a0727a7eff0d8e5297534f47393f62bfb30b
hotpotato <ramilreading@gmail.com>
2026-07-22 14:08:55 +0300

    chore(celery beat): schedule config

--- src/app/core/celery.py
+++ src/app/core/celery.py
@@ -16,8 +16,12 @@ celery_app.autodiscover_tasks(["src.app.tasks"])
 celery_app.conf.update(
     beat_schedule={
         "delete_expired_urls": {
-            "task": "src.app.tasks.delete_expired",
+            "task": "src.app.tasks.delete_expired_task",
             "schedule": crontab(hour=3, minute=0),
+        },
+        "replenish_credits": {
+            "task": "src.app.tasks.replenish_credits_task",
+            "schedule": crontab(day_of_month=1, hour=0, minute=0),
         },
     },
     timezone="UTC",
