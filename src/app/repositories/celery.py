7bd0edf6f9cab557ef830ef016cc50bcac9fca53
Hotpotato89 <ramilreading@gmail.com>
2026-07-22 14:21:11 +0300

    Feature/replenish credits (#47)
    * feat(celery repo): replenish credits method
    
    * feat(celety tasks): replenish task
    
    * style: used ruff
    
    * chore(celery beat): schedule config
    
    * docs: Update README
    
    * chore: relock uv

--- src/app/repositories/celery.py
+++ src/app/repositories/celery.py
@@ -1,13 +1,14 @@
 from datetime import datetime, timezone
-from typing import Literal
 
-from sqlalchemy import delete
+from sqlalchemy import delete, update
 from sqlalchemy.orm import Session
 
+from src.app.core.enums import ExportFormat
 from src.app.core.exceptions import SlugNotFoundError
 from src.app.models.click import Click
 from src.app.models.export_log import ExportLog
 from src.app.models.short_url import ShortUrl
+from src.app.models.user import User
 
 
 class CeleryRepository:
@@ -31,9 +32,7 @@ class CeleryRepository:
         else:
             return 0
 
-    def save_export_logs(
-        self, user_id: int, format: Literal["csv", "json", "xlsx"]
-    ) -> None:
+    def save_export_logs(self, user_id: int, format: ExportFormat) -> None:
         new_log = ExportLog(user_id=user_id, format=format)
         self.session.add(new_log)
         self.session.commit()
@@ -41,4 +40,9 @@ class CeleryRepository:
     def save_click(self, url_id: int, user_ip: str, user_agent: str) -> None:
         new_click = Click(url_id=url_id, user_ip=user_ip, user_agent=user_agent)
         self.session.add(new_click)
+        self.session.commit()
+
+    def replenish_credits(self, amount: int) -> None:
+        stmt = update(User).values(credits=User.credits + amount)
+        self.session.execute(stmt)
         self.session.commit()
