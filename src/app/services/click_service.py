404d47fbe20afd8460813ea635edb292add8e842
hotpotato89 <ramilreading@gmail.com>
2026-07-25 16:01:35 +0300

    feat(click service): pagination logic

--- src/app/services/click_service.py
+++ src/app/services/click_service.py
@@ -5,6 +5,7 @@ from src.app.core.exceptions import PermissionDeniedError
 from src.app.repositories.click import ClickRepository
 from src.app.repositories.short_url_repository import ShortUrlRepository
 from src.app.schemas.click import ClickResponse
+from src.app.schemas.pagination import CursorPaginationResponse
 
 
 class ClickService:
@@ -13,13 +14,24 @@ class ClickService:
         self.url_repo = url_repo
 
     async def get_stats(
-        self, user_id: int, user_role: UserRole, url_id: int
-    ) -> Sequence[ClickResponse]:
+        self, user_id: int, user_role: UserRole, url_id: int,
+        limit: int = 10, cursor: int | None = None
+    ) -> CursorPaginationResponse[ClickResponse]:
         url = await self.url_repo.get_url_by_id(url_id)
 
         if url.owner_id != user_id and user_role != "admin":
             raise PermissionDeniedError("You dont have permission to view it")
 
-        stats = await self.repo.get_clicks_by_url_id(url.id)
+        db_result = await self.repo.get_clicks_by_url_id(url.id, limit, cursor)
+        has_more = len(db_result) > limit
+        next_cursor = db_result[limit - 1].id if has_more else None
 
-        return [ClickResponse.model_validate(stat) for stat in stats]
+        items = db_result[:limit]
+
+        return CursorPaginationResponse(
+            items=[ClickResponse.model_validate(item) for item in items],
+            next_cursor=next_cursor,
+            limit=limit,
+            has_more=has_more
+        )
+        
