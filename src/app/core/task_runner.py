3de5f684674ff7d5fd91d874e43ff36a507794bc
hotpotato89 <ramilreading@gmail.com>
2026-07-26 12:25:12 +0300

    feat(task runner): async version

--- src/app/core/task_runner.py
+++ src/app/core/task_runner.py
@@ -1,6 +1,6 @@
 from typing import Callable
 
-from src.app.core.celery import celery_app  # noqa
+from src.app.core.taskiq_broker import broker # noqa
 from src.app.core.logging import get_logger
 
 logger = get_logger(__name__)
@@ -8,10 +8,10 @@ logger = get_logger(__name__)
 
 class TaskRunner:
     @staticmethod
-    def run_in_bg(task: Callable, *args, **kwargs) -> None:
+    async def run_in_bg(task: Callable, *args, **kwargs) -> None:
 
         try:
-            result = task.delay(*args, **kwargs)
+            result = await task.kiq(*args, **kwargs)
             logger.debug("Task sent!", id=result.id)
         except Exception as e:
             logger.error("Failed to send task", error=str(e), exc_info=True)
