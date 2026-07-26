5a4b70b0d39f96e30e8e2a73c83e20c65dbb3e61
hotpotato89 <ramilreading@gmail.com>
2026-07-26 12:22:54 +0300

    style: used ruff

--- src/app/core/database.py
+++ src/app/core/database.py
@@ -1,5 +1,3 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, Session
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from src.app.core.settings import settings
 
