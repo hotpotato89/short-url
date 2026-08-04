03c569eb535affbb325171ef9702fe20b0808ff4
hotpotato89 <ramilreading@gmail.com>
2026-08-04 19:45:38 +0300

    refactor(base.py): BaseTimestamped

--- src/app/models/base.py
+++ src/app/models/base.py
@@ -1,5 +1,11 @@
 from sqlalchemy.orm import DeclarativeBase
 
+from src.app.models.mixins import IdPkMixin, TimestampMixin
 
-class Base(DeclarativeBase):
+
+class Base(DeclarativeBase, IdPkMixin):
+    pass
+
+
+class BaseTimestamped(Base, TimestampMixin):
     pass
