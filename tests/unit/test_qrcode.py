c8f8e19a3bfa2f55ee34a6f66ca945f3699e4bc8
hotpotato89 <ramilreading@gmail.com>
2026-08-05 21:23:37 +0300

    tests: qr codes direction

--- tests/unit/test_qrcode.py
+++ tests/unit/test_qrcode.py
@@ -1,12 +1,35 @@
-import pytest
+import base64
+from io import BytesIO
+from typing import Final
 
+import pytest
+import qrcode
+from PIL import Image
+from pyzbar.pyzbar import decode
+
+from src.app.core.settings import settings
 from src.app.utils.qrcode import generate_qr_base64
+
+EXAMPLE_URL: Final[str] = "https://example.com"
 
 
 async def test_qr_code_is_str() -> None:
-    data = "https://example.com"
-    result = generate_qr_base64(data)
+    result = generate_qr_base64(EXAMPLE_URL)
 
     assert isinstance(result, str)
     assert len(result) > 0
     assert result.startswith("iVBORw0KGgo")  # PNG signature in base64
+
+
+async def test_qr_code_direction() -> None:
+    result = generate_qr_base64(EXAMPLE_URL)
+    byte = base64.b64decode(result)
+    image = Image.open(BytesIO(byte))
+
+    decoded_data = decode(image)
+    assert len(decoded_data) == 1
+
+    actual_url = decoded_data[0].data.decode()
+    expected_url = EXAMPLE_URL
+
+    assert actual_url == expected_url
